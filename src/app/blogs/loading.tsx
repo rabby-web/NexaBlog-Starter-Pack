e33f50578cf1f55844b70431a6aa267bbd5cc120
rabby-web <rabby.webdeveloper@gmail.com>
2025-02-06 18:51:29 +0600

    blogs page blog display

---
+++ src/app/blogs/loading.tsx
@@ -0,0 +1,11 @@
+import Spinner from "@/components/ui/Spinner";
+
+const BlogsLoadingPage = () => {
+  return (
+    <div>
+      <Spinner></Spinner>
+    </div>
+  );
+};
+
+export default BlogsLoadingPage;
