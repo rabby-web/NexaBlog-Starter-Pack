63faeafb7e0e0062378157803dc2c04be2182c60
rabby-web <rabby.webdeveloper@gmail.com>
2025-02-06 21:44:31 +0600

    create a new blog with server action

---
+++ src/action/createBlogs.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { redirect } from "next/navigation";
+
+export const createBlog = async (data: FormData) => {
+  const blogData = Object.fromEntries(data.entries());
+//   console.log(33, blogData);
+  const res = await fetch("http://localhost:5000/blogs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(blogData),
+  });
+
+  const blogInfo = await res.json();
+  if (blogInfo) {
+    redirect(`/blogs/${blogInfo.id}`);
+  }
+  return blogInfo;
+};
