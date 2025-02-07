2d0261b66ff015eba304db0b427dd7d27e0e38de
rabby-web <rabby.webdeveloper@gmail.com>
2025-02-07 10:53:49 +0600

    nexa blog project done

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,16 +1,26 @@
+"use client";
+
 import BlogCard from "@/components/ui/BlogCard";
+import Spinner from "@/components/ui/Spinner";
+import { useGetBlogsQuery } from "@/redux/apis/blogs.slice";
 import { Blog } from "@/types";
-import { Metadata } from "next";
+// import { Metadata } from "next";
 
-export const metaData: Metadata = {
-  title: "NextBlog | Blogs",
-};
+// export const metaData: Metadata = {
+//   title: "NextBlog | Blogs",
+// };
 
-const BlogsPage = async () => {
-  const res = await fetch("http://localhost:5000/blogs", {
-    cache: "no-store",
-  });
-  const blogs = await res.json();
+const BlogsPage = () => {
+  // const res = await fetch("http://localhost:5000/blogs", {
+  //   cache: "no-store",
+  // });
+  // const blogs = await res.json();
+
+  const { data: blogs, isLoading } = useGetBlogsQuery({});
+  if (isLoading) {
+    return <Spinner></Spinner>;
+  }
+
   return (
     <div className="w-[90%] mx-auto">
       <h1 className="text-3xl text-center my-5 font-bold">
